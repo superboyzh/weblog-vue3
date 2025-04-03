93b52cde8ff45fbf77cba941dd0a7ff49063c1e1
zenghao <zengh@shjysoft.com>
2025-04-03 17:59:03 +0800

    feat：引入 Element Plus 图标库和 animate.css，更新登录页样式

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@
 import "@/assets/main.css";
 import { createApp } from "vue";
 import App from "@/App.vue";
+import "animate.css";
 // 导入路由
 import router from "@/router";
 
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
 const app = createApp(App);
+// 引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
 
 // 应用路由
 app.use(router);
