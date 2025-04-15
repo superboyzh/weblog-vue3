959e643225ef1b2b35a741279e37e6bce9192d67
zenghao <zengh@shjysoft.com>
2025-04-15 17:58:05 +0800

    初始化页面布局

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,12 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 import Login from "@/pages/admin/login.vue";
 import AdminIndex from "@/pages/admin/index.vue";
+import Admin from "@/layouts/admin/admin.vue";
+
+import AdminArticleList from "@/pages/admin/article-list.vue";
+import AdminCategoryList from "@/pages/admin/category-list.vue";
+import AdminTagList from "@/pages/admin/tag-list.vue";
+import AdminBlogSetting from "@/pages/admin/blog-setting.vue";
 
 // 统一在这里声明所有路由
 const routes = [
@@ -22,11 +28,45 @@ const routes = [
     },
   },
   {
-    path: "/admin/index", // 登录页
-    component: AdminIndex,
-    meta: {
-      title: "Admin 后台首页",
-    },
+    path: "/admin", // 后台管理路由
+    component: Admin, // 后台布局组件
+    children: [
+      {
+        path: "/admin/index",
+        component: AdminIndex, // 主内容区域，具体需要渲染的页面
+        meta: {
+          title: "Admin 后台首页",
+        },
+      },
+      {
+        path: "/admin/article/list",
+        component: AdminArticleList,
+        meta: {
+          title: "文章管理",
+        },
+      },
+      {
+        path: "/admin/category/list",
+        component: AdminCategoryList,
+        meta: {
+          title: "分类管理",
+        },
+      },
+      {
+        path: "/admin/tag/list",
+        component: AdminTagList,
+        meta: {
+          title: "标签管理",
+        },
+      },
+      {
+        path: "/admin/blog/setting",
+        component: AdminBlogSetting,
+        meta: {
+          title: "博客设置",
+        },
+      },
+    ],
   },
 ];
 
