b92ffd28de18b1098a29b36964dd0ae865dcb435
zenghao <zengh@shjysoft.com>
2025-04-16 11:16:31 +0800

    引入pinia

--- src/main.js
+++ src/main.js
@@ -12,6 +12,10 @@ import "@/permission";
 
 // 导入 Element Plus 图标
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+// 引入全局状态管理 Pinia
+import { createPinia } from "pinia";
+
+const pinia = createPinia();
 
 const app = createApp(App);
 // 引入图标
@@ -21,4 +25,5 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 
 // 应用路由
 app.use(router);
+app.use(pinia);
 app.mount("#app");
